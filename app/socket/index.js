e68873b945a7736116a227e75263c559c6c15f36
Bodo Kaiser <i@bodokaiser.io>
2014-01-12 12:15:39 +0100

    applied certificate to https server

--- app/socket/index.js
+++ app/socket/index.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
 
 function createWebSocket() {
     var path = url.format({
-        protocol: 'ws',
+        protocol: 'wss',
         host: location.host
     });
 
